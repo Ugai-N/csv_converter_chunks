dc968e5168c9475c1e2f515153cdadf569845551
Nadezhda Ugay <777ugay@gmail.com>
2024-03-24 21:51:30 +0200

    fix

--- main.py
+++ main.py
@@ -11,22 +11,23 @@ from utils import generate_csv_schema
 def main():
     now = datetime.now()
     file_name = str(now).replace(" ", "_").replace(":", "-")[:19]
-    file_name = 'one_mil_2'
-    file_name = 'test100'
+
     output_path = Path(__file__).resolve().parent.joinpath('data', f'{file_name}')
     file_csv = Path(__file__).resolve().parent.joinpath('data', f'{str(file_name)}.csv')
-    file_csv = 'data_dev/one_mil_2.csv'
-    file_csv = 'data_dev/test100.csv'
 
-    # generate_csv_schema(100000, file_csv)
+    # generates csv with the stated n of rows
+    generate_csv_schema(100000, file_csv)
 
+    # choose the converter type
     converter = ConverterProcessing(file_csv, output_path)
     # converter = ConverterThreading(file_csv, output_path)
     # converter = ConverterSerial(file_csv, output_path)
     # converter = ConverterThreadingParallel(file_csv, output_path)
 
+    # initialize converter, input chunksize
     converter.run(8_000)
 
+    # prints the info on convertion process
     converter.info()
 
     # converter.convert_back()
